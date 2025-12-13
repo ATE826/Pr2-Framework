e88235ee79c989f1a5b52c0bd20f6943c6cb164d
Anton Tseysnovskiy <antontseysnovskiy@gmail.com>
2025-12-13 12:41:42 +0300

    api-gateway update

--- service_users/internal/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	ID       string   `json:"id"`
-	Email    string   `json:"email"`
-	Password string   `json:"password"`
-	Name     string   `json:"name"`
-	Roles    []string `json:"roles"`
-}
